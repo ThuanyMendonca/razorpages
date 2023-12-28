e550c860d136ad74f0747db0cabf8c1372d99791
thuany mendonca <thuany1997@hotmail.com>
2023-12-28 09:45:08 -0300

    Add razor pages crud

---
+++ RazorPagesFilme/Data/RazorPagesFilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesFilme.Models;
+
+namespace RazorPagesFilme.Data
+{
+    public class RazorPagesFilmeContext : DbContext
+    {
+        public RazorPagesFilmeContext (DbContextOptions<RazorPagesFilmeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesFilme.Models.Filme> Filme { get; set; } = default!;
+    }
+}
---
+++ RazorPagesFilme/Pages/Filmes/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesFilme.Data;
+using RazorPagesFilme.Models;
+
+namespace RazorPagesFilme.Pages.Filmes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesFilme.Data.RazorPagesFilmeContext _context;
+
+        public CreateModel(RazorPagesFilme.Data.RazorPagesFilmeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Filme Filme { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Filme == null || Filme == null)
+            {
+                return Page();
+            }
+
+            _context.Filme.Add(Filme);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
